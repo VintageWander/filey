4e6757e7d85ec2d95c4fd4f8bf862b278d955b1a
Giang <nguyendtgiang@gmail.com>
2024-10-22 13:44:36 +0700

    added comments for documentation

--- src-tauri/src/info/commands.rs
+++ src-tauri/src/info/commands.rs
@@ -6,9 +6,12 @@ use crate::error::Error;
 
 #[tauri::command]
 pub async fn database_ready(app_handle: AppHandle) -> bool {
+    // Resolve the path to the database file
+    // If it fails -> database not ready
     let Ok(path) = app_handle.path().resolve("data.db", BaseDirectory::AppData) else {
         return false;
     };
+    // Try to open the file, if it does not -> database not ready
     app_handle
         .fs()
         .open(path, OpenOptions::new().read(true).clone())
@@ -17,22 +20,29 @@ pub async fn database_ready(app_handle: AppHandle) -> bool {
 
 #[tauri::command]
 pub fn local_ips() -> Result<Vec<String>, Error> {
-    let local_ips = list_afinet_netifas()?
+    let local_ips = 
+    // List all network interfaces
+    list_afinet_netifas()?
+        // Goes through each of them
         .iter()
+        // Only takes the ip address, ignore the interface name (each item is a pair of name:ip)
         .map(|(_, ip)| ip.to_string())
+        // Keeps the private ip addresses, those starts with 10.0, 172.16 and 192.168
         .filter(|ip| {
             ip.starts_with("10.0") || ip.starts_with("172.16") || ip.starts_with("192.168")
         })
+        // collect them into a list
         .collect();
 
     Ok(local_ips)
 }
 
-// Neat trick to determine whether or not a device is a laptop or desktop
-// Though this could be bypassed easily by just use a laptop without a battery, powered from wall outlet
-// but the question is, why do you have to?
+// Neat trick to determine whether or not a device is a laptop, mobile or desktop
+// Though this could be bypassed easily by using a laptop without a battery.
+// Technically speaking a laptop that does not have battery, lives on a desktop, pretty much is a desktop at this point
 #[tauri::command]
 pub fn check_battery() -> bool {
+    // On devices that are NOT mobile -> checks for battery's existence
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
     {
         let Ok(manager) = battery::Manager::new() else {
@@ -44,6 +54,7 @@ pub fn check_battery() -> bool {
 
         !batteries.into_iter().collect::<Vec<_>>().is_empty()
     }
+    // On devices that ARE mobile -> just return true, they all have batteries
     #[cfg(any(target_os = "android", target_os = "ios"))]
     {
         true
