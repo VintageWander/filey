2de87f4a7f5e15c435822a760595b3023b57a925
Giang <nguyendtgiang@gmail.com>
2024-10-22 16:09:26 +0700

    added function that prints local machine name

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
+import { OsType } from "./models";
+
 export const capitalLetter = (str: string): string => {
   return str[0].toUpperCase() + str.slice(1);
 };
+
+export const printLocalMachineName = (host: OsType, existsBattery: boolean) =>
+  `This ${
+    host === "macos" || host === "windows" || host === "linux"
+      ? existsBattery
+        ? " laptop"
+        : " desktop"
+      : host === "android" || host === "ios"
+      ? " phone"
+      : " unknown device"
+  }`;
