eecee6dd003d6b33a513c487fad67ef09748ec40
Giang <nguyendtgiang@gmail.com>
2024-10-22 12:50:35 +0700

    added database file creation and connect

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,10 @@
+mod db;
 mod error;
 
 use log::LevelFilter;
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
+use tauri::{path::BaseDirectory, Manager};
 use tokio::sync::{oneshot::Sender, Mutex};
 
 pub struct AppState {
@@ -16,7 +18,6 @@ pub struct ServerResponse<T: Serialize> {
     pub data: T,
 }
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -29,10 +30,26 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_log::Builder::new()
-                .level(LevelFilter::Error)
+                .level(LevelFilter::Info)
                 .build(),
         )
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let path = app
+                .path()
+                .resolve("data.db", BaseDirectory::AppData)?
+                .display()
+                .to_string();
+
+            let db = tauri::async_runtime::block_on(db::connect_and_migrate(path));
+
+            app.manage(AppState {
+                db,
+                shutdown_sender: Mutex::new(None),
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("Application failed to start");
