3833b20baad7aa0f28e075e75f918fe196b7de36
Giang <nguyendtgiang@gmail.com>
2024-10-22 16:09:59 +0700

    added plugin os to application

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,6 +25,7 @@ pub struct ServerResponse<T: Serialize> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init())
