b8802ab9272496832d3228789af532720d5bb3ae
Giang <nguyendtgiang@gmail.com>
2024-10-22 13:44:22 +0700

    implement functionality to get current ip address

--- src/components/ServerConfig/index.tsx
+++ src/components/ServerConfig/index.tsx
@@ -1,15 +1,24 @@
 import { Button, Group, Stack, Text } from "@mantine/core";
 import { useAtom } from "jotai";
-import { connectedToAtom, serverStatusAtom } from "../../store";
+import { connectedToAtom, localIpsAtom, serverStatusAtom } from "../../store";
 import { capitalLetter } from "../../utils";
 import { FaWifi } from "react-icons/fa6";
 import { FiCloud, FiCloudOff } from "react-icons/fi";
+import { useEffect } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { error } from "@tauri-apps/plugin-log";
 
 export const ServerConfig = () => {
   const [connectedTo] = useAtom(connectedToAtom);
   const [serverStatus, setServerStatus] = useAtom(serverStatusAtom);
+  const [localIps, setLocalIps] = useAtom<string[]>(localIpsAtom);
 
+  const isLocal = connectedTo === "This machine";
   const isOnline = serverStatus === "online";
+
+  useEffect(() => {
+    invoke<string[]>("local_ips").then(setLocalIps).catch(error);
+  }, []);
 
   return (
     <Stack>
@@ -31,7 +40,15 @@ export const ServerConfig = () => {
       </Group>
       <Group justify="space-between">
         <Text>Local IP addresses:</Text>
-        <Text>192.168.1.1</Text>
+        {isOnline ? (
+          <Stack>
+            {localIps.map((ip) => (
+              <code key={ip}>{ip}:38899</code>
+            ))}
+          </Stack>
+        ) : (
+          "Currently offline"
+        )}
       </Group>
     </Stack>
   );
