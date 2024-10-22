da49bc165b6a94708066ee0b42bbc41167db6046
Giang <nguyendtgiang@gmail.com>
2024-10-22 12:38:23 +0700

    add comments to store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,9 @@
 import { atom } from "jotai";
 import { FileModel } from "./models";
 
+// File list
 export const filesAtom = atom<FileModel[]>([]);
 
+// Server config related store
 export const connectedToAtom = atom<string>("This machine");
+export const serverStatusAtom = atom<"online" | "offline">("offline");
