01a1760c01e53bbe70b1fa1ede9e1000854917f3
Giang <nguyendtgiang@gmail.com>
2024-10-22 14:40:57 +0700

    mount FileList component to main screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "@mantine/core/styles.css";
 import { Container, Divider, MantineProvider } from "@mantine/core";
 import { MenuBar } from "./components/MenuBar";
 import { ServerConfig } from "./components/ServerConfig";
+import { FileList } from "./components/FileList";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Divider label="Server Config" my="lg" />
         <ServerConfig />
         <Divider label="File list" my="lg" />
+        <FileList />
       </Container>
     </MantineProvider>
   );
